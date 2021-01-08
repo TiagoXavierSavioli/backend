685b7dc67740d706a9e45a16c16e2b781dd7ed6e
TiagoXavierSavioli <71526510+TiagoXavierSavioli@users.noreply.github.com>
2021-01-07 22:26:36 -0300

    Update User.js

--- src/Models/User.js
+++ src/Models/User.js
@@ -11,11 +11,9 @@ const Schema = new mongoose.Schema({
     },
     name: {
         type: String,
-        required: true
     },
     age: {
         type: String,
-        required: true
     },
     description: {
         type: String,
